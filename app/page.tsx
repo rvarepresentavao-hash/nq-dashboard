0b7b1a66197affde0097eb9703ca6ef24262207f
Renan <renan_alcantara@ymail.com>
2026-05-10 19:07:47 -0300

    fix page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 export default function Home() {
   return (
-    <div style={{
-      background:"#071018",
-      color:"white",
-      minHeight:"100vh",
-      padding:"40px",
-      fontFamily:"Arial"
-    }}>
-      <h1 style={{
-        color:"#00ff88",
-        fontSize:"42px"
-      }}>
+    <div
+      style={{
+        background: "#071018",
+        color: "white",
+        minHeight: "100vh",
+        padding: "40px",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1
+        style={{
+          color: "#00ff88",
+          fontSize: "42px",
+        }}
+      >
         NQ CME ALPHA ELITE
       </h1>
 
